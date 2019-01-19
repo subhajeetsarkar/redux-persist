66570de37b399147adf29411eb744e680039fa3a
SjSorc <Techaccsj@gmail.com>
2019-01-19 16:10:05 +0530

    Revert "Merge branch 'master' of https://github.com/subhajeetsarkar/redux-persist"
    This reverts commit 32f660835fb5d4f462433138f1f4470a12489273, reversing
    changes made to 99338087a24874927946bd6062f27e70a87a63ed.

--- src/persistStore.js
+++ src/persistStore.js
@@ -1,82 +1,52 @@
-'use strict'
+// @flow
 
-exports.__esModule = true
+import type {
+  Persistor,
+  PersistConfig,
+  PersistorOptions,
+  PersistorState,
+  MigrationManifest,
+  RehydrateAction,
+  RehydrateErrorType,
+} from './types'
 
-var _extends =
-  Object.assign ||
-  function(target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i]
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key]
-        }
-      }
-    }
-    return target
-  }
+import { createStore } from 'redux'
+import persistReducer from './persistReducer'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from './constants'
 
-exports.default = persistStore
+type PendingRehydrate = [Object, RehydrateErrorType, PersistConfig]
+type Persist = <R>(PersistConfig, MigrationManifest) => R => R
+type CreatePersistor = Object => void
+type BoostrappedCb = () => any
 
-var _redux = require('redux')
-
-var _persistReducer = require('./persistReducer')
-
-var _persistReducer2 = _interopRequireDefault(_persistReducer)
-
-var _constants = require('./constants')
-import { default as storage } from 'react-native-couchbase-lite'
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj }
-}
-
-function _toConsumableArray(arr) {
-  if (Array.isArray(arr)) {
-    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-      arr2[i] = arr[i]
-    }
-    return arr2
-  } else {
-    return Array.from(arr)
-  }
-}
-
-var initialState = {
+const initialState: PersistorState = {
   registry: [],
   bootstrapped: false,
 }
 
-var persistorReducer = function persistorReducer() {
-  var state =
-    arguments.length > 0 && arguments[0] !== undefined
-      ? arguments[0]
-      : initialState
-  var action = arguments[1]
-
+const persistorReducer = (state = initialState, action) => {
   switch (action.type) {
-    case _constants.REGISTER:
-      return _extends({}, state, {
-        registry: [].concat(_toConsumableArray(state.registry), [action.key]),
-      })
-    case _constants.REHYDRATE:
-      var firstIndex = state.registry.indexOf(action.key)
-      var registry = [].concat(_toConsumableArray(state.registry))
+    case REGISTER:
+      return { ...state, registry: [...state.registry, action.key] }
+    case REHYDRATE:
+      let firstIndex = state.registry.indexOf(action.key)
+      let registry = [...state.registry]
       registry.splice(firstIndex, 1)
-      return _extends({}, state, {
-        registry: registry,
-        bootstrapped: registry.length === 0,
-      })
+      return { ...state, registry, bootstrapped: registry.length === 0 }
     default:
       return state
   }
 }
 
-function persistStore(store, options, cb) {
+export default function persistStore(
+  store: Object,
+  options?: ?PersistorOptions,
+  cb?: BoostrappedCb
+): Persistor {
   // help catch incorrect usage of passing PersistConfig in as PersistorOptions
   if (process.env.NODE_ENV !== 'production') {
-    var optionsToTest = options || {}
-    var bannedKeys = [
+    let optionsToTest: Object = options || {}
+    let bannedKeys = [
       'blacklist',
       'whitelist',
       'transforms',
@@ -84,37 +54,35 @@ function persistStore(store, options, cb) {
       'keyPrefix',
       'migrate',
     ]
-    bannedKeys.forEach(function(k) {
+    bannedKeys.forEach(k => {
       if (!!optionsToTest[k])
         console.error(
-          'redux-persist: invalid option passed to persistStore: "' +
-            k +
-            '". You may be incorrectly passing persistConfig into persistStore, whereas it should be passed into persistReducer.'
+          `redux-persist: invalid option passed to persistStore: "${k}". You may be incorrectly passing persistConfig into persistStore, whereas it should be passed into persistReducer.`
         )
     })
   }
-  var boostrappedCb = cb || false
+  let boostrappedCb = cb || false
 
-  var _pStore = (0, _redux.createStore)(
+  let _pStore = createStore(
     persistorReducer,
     initialState,
     options ? options.enhancer : undefined
   )
-  var register = function register(key) {
+  let register = (key: string) => {
     _pStore.dispatch({
-      type: _constants.REGISTER,
-      key: key,
+      type: REGISTER,
+      key,
     })
   }
 
-  var rehydrate = function rehydrate(key, payload, err) {
-    var rehydrateAction = {
-      type: _constants.REHYDRATE,
-      payload: payload,
-      err: err,
-      key: key,
-      // dispatch to `store` to rehydrate and `persistor` to track result
+  let rehydrate = (key: string, payload: Object, err: any) => {
+    let rehydrateAction = {
+      type: REHYDRATE,
+      payload,
+      err,
+      key,
     }
+    // dispatch to `store` to rehydrate and `persistor` to track result
     store.dispatch(rehydrateAction)
     _pStore.dispatch(rehydrateAction)
     if (boostrappedCb && persistor.getState().bootstrapped) {
@@ -123,57 +91,39 @@ function persistStore(store, options, cb) {
     }
   }
 
-  var persistor = _extends({}, _pStore, {
-    purge: function purge() {
-      var results = []
+  let persistor: Persistor = {
+    ..._pStore,
+    purge: () => {
+      let results = []
       store.dispatch({
-        type: _constants.PURGE,
-        result: function result(purgeResult) {
+        type: PURGE,
+        result: purgeResult => {
           results.push(purgeResult)
         },
       })
       return Promise.all(results)
     },
-    flush: function flush() {
-      var results = []
+    flush: () => {
+      let results = []
       store.dispatch({
-        type: _constants.FLUSH,
-        result: function result(flushResult) {
+        type: FLUSH,
+        result: flushResult => {
           results.push(flushResult)
         },
       })
       return Promise.all(results)
     },
-    pause: function pause() {
+    pause: () => {
       store.dispatch({
-        type: _constants.PAUSE,
+        type: PAUSE,
       })
     },
-    persist: function persist() {
-      store.dispatch({
-        type: _constants.PERSIST,
-        register: register,
-        rehydrate: rehydrate,
-      })
+    persist: () => {
+      store.dispatch({ type: PERSIST, register, rehydrate })
     },
-  })
+  }
 
-  storage
-    .getLocalDocument('AGENT_DATA')
-    .then(data => {
-      storage
-        .initialiseDBWithAgentId(data)
-        .then(() => {
-          persistor.persist()
-        })
-        .catch(() => {
-          //Todo proper handling
-          persistor.persist()
-        })
-    })
-    .catch(() => {
-      //Todo proper handling
-      persistor.persist()
-    })
+  persistor.persist()
+
   return persistor
 }
